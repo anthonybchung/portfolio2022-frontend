037bda2b68aeea0bcdf6314a5419e3bca2c60044
Anthony B Chung <99620815+anthonybchung@users.noreply.github.com>
2022-11-29 18:42:29 +1100

    navigation dry

--- src/components/navigationbar/NavigationBar.component.jsx
+++ src/components/navigationbar/NavigationBar.component.jsx
@@ -1,61 +1,57 @@
 import React from "react";
-import { Navbar, Container, Nav, Button } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 
 import "./NavigationBar.styles.scss";
 
 const NavigationBar = () => {
-  const activeStyle = {
-    color: "white",
+  const menuItem = [
+    {
+      path: "/side-projects",
+      name: "Side Projects",
+    },
+    {
+      path: "/blog",
+      name: "Blog",
+    },
+    {
+      path: "/about-me",
+      name: "About Me",
+    },
+    {
+      path: "/login",
+      name: "Login",
+    },
+  ];
+
+  const isActiveStyle = {
     fontWeight: "bolder",
     textDecoration: "underline",
   };
 
   return (
-    <Navbar bg="dark" variant="dark" expand="md">
-      <Container>
-        <NavLink to="/" className="navigation-link">
-          Home
-        </NavLink>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="ms-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
-            {/* <NavLink
-              className="navigation-link"
-              style={({ isActive }) => (isActive ? activeStyle : undefined)}
-              to="/side-projects"
-            >
-              Side Projects
-            </NavLink> */}
-            <NavLink
-              className="navigation-link"
-              style={({ isActive }) => (isActive ? activeStyle : undefined)}
-              to="/blog"
-            >
-              Blog
-            </NavLink>
-            <NavLink
-              className="navigation-link"
-              style={({ isActive }) => (isActive ? activeStyle : undefined)}
-              to="/about-me"
-            >
-              About Me
-            </NavLink>
-          </Nav>
-          {/* <NavLink
-            to="/login"
-            style={({ isActive }) => (isActive ? activeStyle : undefined)}
-            className=" navigation-link d-flex"
-          >
-            Login
-  </NavLink>*/}
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <nav>
+      <div className="nav-container">
+        <div className="logo">
+          <NavLink to="/" className="nav-item">
+            Home
+          </NavLink>
+        </div>
+        <div className="menu-item">
+          {menuItem.map((item, index) => {
+            return (
+              <NavLink
+                to={item.path}
+                key={index}
+                className="nav-item"
+                style={({ isActive }) => (isActive ? isActiveStyle : undefined)}
+              >
+                {item.name}
+              </NavLink>
+            );
+          })}
+        </div>
+      </div>
+    </nav>
   );
 };
 
